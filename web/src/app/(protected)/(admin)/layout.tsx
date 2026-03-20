39307393b111ccaa2b343aec345abf66654df7ae
Evgeniy Lebedev <lebedev1537@gmail.com>
2026-03-21 02:53:55 +0400

    Improvements for sheets

--- web/src/app/(protected)/(admin)/layout.tsx
+++ web/src/app/(protected)/(admin)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { RoleGuard } from '@/components/user/role-guard'
 import { SidebarInset } from '@/components/ui/sidebar'
 import { AdminAppSidebar } from '@/components/common/sidebar/admin-app-sidebar'
+import { MobileSidebarTrigger } from '@/components/common/sidebar/mobile-sidebar-trigger'
 
 export default function AdminLayout({
   children,
@@ -11,7 +12,8 @@ export default function AdminLayout({
   return (
     <RoleGuard role="ADMIN">
       <AdminAppSidebar />
-      <SidebarInset className="h-full overflow-hidden">
+      <SidebarInset className="h-full overflow-hidden flex flex-col">
+        <MobileSidebarTrigger />
         {children}
       </SidebarInset>
     </RoleGuard>
